d16dc038ce5904d2984ffdc0240f12df296984ad
Koding Dev <kodingdev@pm.me>
2021-04-11 11:38:52 +1000

    :apple: Added Dockerfile to create ISO on macOS

--- tools/scripts/mkiso.sh
+++ tools/scripts/mkiso.sh
@@ -17,7 +17,11 @@ cp tools/boot/grub/grub.cfg build/iso/boot/grub/grub.cfg
 cp cmake-build-debug/cosmicOS build/iso/boot/kernel.bin
 
 # Make ISO
-grub-mkrescue -o build/output/cosmicOS.iso build/iso
+if [ $1 = 'docker' ]; then
+  docker run -v $(pwd)/build/output:/iso -v $(pwd)/build/iso:/build cosmicos:image grub-mkrescue -o /iso/cosmicOS.iso /build
+else
+  grub-mkrescue -o build/output/cosmicOS.iso build/iso
+fi
 
 # Cleanup
 rm -rf build/iso
